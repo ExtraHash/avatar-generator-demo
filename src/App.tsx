b14b07f6b902c51866714fe97076b17d0b665131
ExtraHash <ExtraHash@protonmail.com>
2021-01-23 09:43:21 -0700

    add download button

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={avatar} className="App-logo" alt="logo" />
+        <br />
+        <br />
+        <button onClick={() => { download(avatar) }}>Download</button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+function download(avatarSrc: string) {
+  var element = document.createElement("a");
+  element.setAttribute("href", avatarSrc);
+  element.setAttribute("download", "avatar.svg");
+
+  element.style.display = "none";
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
